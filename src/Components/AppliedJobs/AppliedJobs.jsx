a132c8c84fdf9082418a6778ce769c721f0b2f23
Sazzadul karim <sazzadulkarim352@gmail.com>
2024-07-02 16:24:48 +0600

    Display Applied Jobs From Stored Ids

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../../Utility/localStorage";
+import { list } from "postcss";
 
 
 const AppliedJobs = () => {
+    const jobs = useLoaderData();
+    const [appliedJobs, setAppliedjobs] = useState([]);
+    const [displayJobs,setDisplayJobs] = useState([]);
+
+    useEffect(()=>{
+      const storedJobIds = getStoredJobApplication();
+      if(jobs.length > 0){
+        // const jobsApplied = jobs.filter (job=>storedJobIds.includes(job.id));
+        const jobsApplied=[];
+        for(const id of storedJobIds){
+            const job = jobs.find(job=>job.id === id);
+            if(job){
+                jobsApplied.push(job);
+            }
+        }
+        setAppliedjobs(jobsApplied);
+        setDisplayJobs(jobsApplied);
+        // console.log(jobs, storedJobIds, jobsApplied);
+      }  
+    },[jobs])
     return (
         <div>
-            <h2>Jobs I applied</h2>
-            
+            <h2 className="text-2xl">Jobs I applied : {appliedJobs.length}</h2>
+            <details className="dropdown">
+  <summary className="btn m-1">Filter by</summary>
+  <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+    <li><a>All</a></li>
+    <li><a>Remote</a></li>
+    <li><a>Onsite</a></li>
+  </ul>
+</details>
+            <ul>
+                {
+                    appliedJobs.map(job=> <li key={job.id}>
+                        <span>{job.job_title} {job.company_name} : {job.remote_or_onsite}</span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,12 +1,19 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../Utility/localStorage";
 
 const JobDetails = () => {
     const jobs = useLoaderData();
     const {id} = useParams();
     const idInt = parseInt(id)
     const job = jobs.find(job=> job.id === idInt);
-    console.log(job);
+    // console.log(job);
+
+    const handleApplyJob =()=>{
+        saveJobApplication(idInt);
+        toast('You have Apply Successfully')
+    }
     return (
         <div>
             <h2>Job Details of : {job.job_title}</h2>
@@ -16,9 +23,10 @@ const JobDetails = () => {
                 </div>
                 <div className="border ">
                     <h2 className="text-2xl"> Side Things</h2>
-                    <button className="btn btn-primary w-full">Apply Now</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
