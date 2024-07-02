44e827d9b8ad257dfd6cb7264ef8de97eeac7a70
Sazzadul karim <sazzadulkarim352@gmail.com>
2024-07-02 11:34:19 +0600

    Add Icons And Handle Show All Jobs

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,4 +1,5 @@
-
+import { SlLocationPin } from "react-icons/sl";
+import { HiOutlineCurrencyDollar } from "react-icons/hi2";
 
 const Job = ({ job }) => {
     const { logo, job_title,company_name,remote_or_onsite,location,job_type,salary} = job
@@ -16,6 +17,10 @@ const Job = ({ job }) => {
                     <div>
                         <button className="btn  px-5 py-2 font-extrabold rounded border-[#7E90FE] text-[#7E90FE]">{remote_or_onsite}</button>
                         <button className="btn m-3 px-5 py-2 font-extrabold rounded border-[#7E90FE]  text-[#7E90FE] ">{job_type}</button>
+                    </div>
+                    <div className="my-4 flex gap-4 items-center">
+<h2 className="flex gap-2"><SlLocationPin className="text-2xl"></SlLocationPin>{location}</h2>
+<h2 className="flex gap-2"><HiOutlineCurrencyDollar className="text-2xl"></HiOutlineCurrencyDollar>{salary}</h2>
                     </div>
                     <div className="card-actions">
                         <button className="btn btn-primary">View Details</button>
